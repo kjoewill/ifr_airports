ceaa3feb9b5a93498c531ab79ec271c0f84bfc23
Kevin W <kjoewill@gmail.com>
2018-09-13 15:38:34 +0000

    captured notes with css usage to scrape name, id and weather support indicator

--- lib/ifr_airports/notes.rb
+++ lib/ifr_airports/notes.rb
@@ -3,4 +3,16 @@
 doc.css('[id = "template-main-content"]').css("table").size
 
 #I believe this is getting me the table I want
-doc.css('[id = "content_dgAirportSearch"]')
+table = doc.css('[id = "content_dgAirportSearch"]')
+
+#This oroduces the second row which is the first content row
+table.css("tr")[1]
+
+#This provides the airport ID from the first row
+table.css("tr")[1].css("td")[0].text
+
+#produces the airport name 
+table.css("tr")[1].css("td")[1].text
+
+#produces bad weather support indicator
+table.css("tr")[1].css("td")[2].text
